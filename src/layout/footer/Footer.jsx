2b89f2f843c27131e880f15ff144cd63bc8ef571
beniamin-hekimian <penig300404@gmail.com>
2025-03-07 21:17:33 +0300

    adding footer section

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import './footer.css';
+import brand from '../../assets/icons/brand.png';
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div>
-        <img src="" alt="" />
-      </div>
-      <div className="footer-section">
-        <h4>Homepage Navigation</h4>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Reservations</li>
-          <li>Order Online</li>
-          <li>Login</li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h4>Contact</h4>
-        <ul>
-          <li>Address</li>
-          <li>Phone number</li>
-          <li>Email</li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h4>Social Media Links</h4>
-        <ul>
-          <li>Address</li>
-          <li>Phone number</li>
-          <li>Email</li>
-        </ul>
+        <img src={brand} alt="Little Lemon brand icon" />
+
+        {/* first links group */}
+        <article className="footer-section">
+          <h4>Navigation</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">Order Online</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </article>
+
+        {/* second links group */}
+        <article className="footer-section">
+          <h4>Contact</h4>
+          <ul>
+            <li><a href="#">+123-456-789</a></li>
+            <li><a href="#">contact@littlelemon.com</a></li>
+            <li><a href="#">Support Center</a></li>
+          </ul>
+        </article>
+
+        {/* third links group */}
+        <article className="footer-section">
+          <h4>Social Media</h4>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+          </ul>
+        </article>
       </div>
     </footer>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Specials />
       <Testimonials />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
