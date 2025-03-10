9214f63f2d3d9b6ccf4a41e9233d58e0ced06746
beniamin-hekimian <penig300404@gmail.com>
2025-03-10 18:21:42 +0300

    responsive dropdown menu navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import { useState, useRef, useEffect } from "react";
 import logo from '../../assets/icons/Logo.svg';
 import './header.css';
 import { Link } from 'react-router-dom';
+import dropdown from '../../assets/icons/dropdown.svg';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <header className="header">
       <div>
+        {/* little lemon logo */}
         <Link to="/">
           <img src={logo} alt="Little Lemon brand logo" />
         </Link>
+        {/* main navbar links */}
         <nav className="navbar">
           <ul>
             <li><Link to="/">Home</Link></li>
@@ -20,6 +37,19 @@ export default function Header() {
             <li><a className="disabled-link custom-button" title="This feature is not available">Login</a></li>
           </ul>
         </nav>
+        {/* hamburger menu icon */}
+        <img src={dropdown} alt="hamburger menu icon" className="hamburger-menu" onClick={() => setIsOpen(!isOpen)} />
+        {/* dropdown navbar links */}
+        {isOpen && (
+          <div className="dropdown-menu" ref={dropdownRef}>
+            <ul>
+              <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+              <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+              <li><Link to="/menu" onClick={() => setIsOpen(false)}>Menu</Link></li>
+              <li><Link to="/booking" onClick={() => setIsOpen(false)}>Reservations</Link></li>
+            </ul>
+          </div>
+        )}
       </div>
     </header>
   )
