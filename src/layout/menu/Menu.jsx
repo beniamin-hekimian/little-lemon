d24ff81d0a2d51229487f02c2905cddd3c524a87
beniamin-hekimian <penig300404@gmail.com>
2025-03-10 11:15:13 +0300

    adding .map() for meals

--- src/layout/menu/Menu.jsx
+++ src/layout/menu/Menu.jsx
@@ -3,86 +3,38 @@ import './menu.css';
 import menu from '../../assets/images/menu.jpg';
 import meal1 from '../../assets/images/special1.jpg';
 
+const meals = [
+  {
+    image: meal1,
+    name: "Greek Salad",
+    price: "$12.99",
+    description:
+      "The famous Greek salad of crispy lettuce, peppers, olives, and our Chicago-style feta cheese, garnished with crunchy garlic and rosemary croutons."
+  }
+];
+
 export default function Menu() {
   return (
     <section className="menu">
-        <div>
-            <img src={menu} alt="restaurant chef" />
-            <h3>Our Menu</h3>
-        </div>
-        <main className="meals-container">
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
-            <main>
-              <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
-              </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-            </main>
-          </article>
+      <div>
+        <img src={menu} alt="A chef preparing food in a restaurant kitchen" />
+        <h3>Our Menu</h3>
+      </div>
 
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
+      <main className="meals-container">
+        {meals.map((meal, index) => (
+          <article className="meal" key={index}>
+            <img src={meal.image} alt={meal.name} />
             <main>
               <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
+                <h4>{meal.name}</h4>
+                <small>{meal.price}</small>
               </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+              <p>{meal.description}</p>
             </main>
           </article>
-
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
-            <main>
-              <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
-              </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-            </main>
-          </article>
-
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
-            <main>
-              <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
-              </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-            </main>
-          </article>
-
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
-            <main>
-              <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
-              </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-            </main>
-          </article>
-
-          {/* the first meal */}
-          <article className="meal">
-            <img src={meal1} alt="Greek salad" />
-            <main>
-              <div>
-                <h4>Greek salad</h4>
-                <small>$12.99</small>
-              </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-            </main>
-          </article>
-        </main>
+        ))}
+      </main>
     </section>
-  )
+  );
 }
