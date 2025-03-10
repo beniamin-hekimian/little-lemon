9776a978dab2dc4a14c66e8343924bf24ad8bb03
beniamin-hekimian <penig300404@gmail.com>
2025-03-10 11:40:56 +0300

    MenuPage maping

--- src/layout/menu/Menu.jsx
+++ src/layout/menu/Menu.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './menu.css';
 import menu from '../../assets/images/menu.jpg';
-import meal1 from '../../assets/images/special1.jpg';
 
 const meals = [
   {
-    image: meal1,
+    image: new URL('../../assets/images/special1.jpg', import.meta.url).href,
     name: "Greek Salad",
     price: "$12.99",
-    description:
-      "The famous Greek salad of crispy lettuce, peppers, olives, and our Chicago-style feta cheese, garnished with crunchy garlic and rosemary croutons."
-  }
+    description: "The famous Greek salad of crispy lettuce, peppers, olives, and our Chicago-style feta cheese, garnished with crunchy garlic and rosemary croutons."
+  },
+  {
+    image: new URL('../../assets/images/special2.jpg', import.meta.url).href,
+    name: "Bruchetta",
+    price: "$5.99",
+    description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
+  },
+  {
+    image: new URL('../../assets/images/special3.jpg', import.meta.url).href,
+    name: "Lemon Dessert",
+    price: "$5.00",
+    description: "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
+  },
 ];
 
 export default function Menu() {
